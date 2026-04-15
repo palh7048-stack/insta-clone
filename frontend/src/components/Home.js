cb943bd0fa98166f9e93316334c88f39ec543184
Harsh <palh7048@gmail.com>
2026-04-15 10:36:26 +0530

    success to fetch comment

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import profile from "../image/profile.jpg";
-import post from "../image/post.jpg";
 import "./Home.css";
+import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+
+  const notify = (message) => toast.error(message);
+  const notifyS = (message) => toast.success(message);
+
   const navigate = useNavigate(); 
   const [data, setData] = useState([]);
   const [comment, setComment] = useState("");
+  const[show, setShow]= useState(false);
+  const[item, setItem] = useState([])
 
   useEffect(() => {
     const token = localStorage.getItem("jwt");
@@ -26,6 +32,20 @@ export default function Home() {
       .then((result) => setData(result))
       .catch((err) => console.log(err));
   }, []);
+
+  // show and hide comment
+  const toggleComment = (posts) =>{
+    if(show){
+      setShow(false)
+      console.log("hide")
+      
+    }else{
+      setShow(true)
+      setItem(posts)
+      console.log(item)
+      console.log("show")
+    }
+  };
 
 const likePost = (id) =>{
   fetch("http://localhost:5000/like", {
@@ -91,7 +111,17 @@ const makeComment = (id,text) =>{
   })
   .then((res) => res.json())
   .then((result) => {
-    console.log(result)
+    const newData = data.map((item) => {
+      if(item._id === result._id){
+        return result
+      }else{
+        return item
+      }
+    })
+    setData(newData)
+    setComment("")
+    notifyS("Comment Posted")
+    
   })    
 }
 
@@ -120,7 +150,8 @@ const makeComment = (id,text) =>{
               {
                 posts.likes.includes(JSON.parse(localStorage.getItem("user"))._id)
                 ? (
-                  <span className="material-symbols-outlined material-symbols-outlined-red"
+                  <span className="material-symbols-outlined 
+                  material-symbols-outlined-red"
               onClick={()=>{unlikePost(posts._id)}}>favorite</span>
                 ):(
                   <span className="material-symbols-outlined"
@@ -129,53 +160,77 @@ const makeComment = (id,text) =>{
               }
               <p>{posts.likes.length} Likes</p>
               <p>{posts.body}</p>
+              <p style={{fontWeight:"bold",cursor:"pointer"}} onClick={()=>{toggleComment(posts)}} >
+                View all Comment</p>
             </div>
 
             {/*add comment*/}
             <div className="add-comment">
+              <span className="material-symbols-outlined">mood</span>
               <input type="text" placeholder="Add a comment" value={comment} onChange={(e)=>{setComment(e.target.value)}} />
               <button className="comment" onClick={()=>{makeComment(posts._id, comment)}}>Post</button>
             </div>
           </div>
-        )
+        );
       })}
 
       {/*show comments */}
-
-      <div className="show-comments">
+        {show && (
+        <div className="show-comments">
         <div className="container">
           <div className="postPic">
-            <img src="http://res.cloudinary.com/dqolrgkwz/image/upload/v1775886242/qzzumrpxdw9ts3q2l2va.jpg"
+            <img src={item.photo}
             alt="post" />
             </div>
             <div className="details">
 
                   {/*card-header*/}
-                <div className="card-header">
+                <div className="card-header"
+                style= {{borderBottom:"1px solid #00000029"}} >
+
                   <div className="card-pic">
                   <img src={profile} alt="profile-image" />
               </div>
-                  <h5>wow</h5> 
+                  <h5>{item.postedBy.name}</h5> 
             </div>
                   {/*comment section */}
-                  <div className="comment-section"></div>
+                  <div className="comment-section" 
+                  style= {{borderBottom:"1px solid #00000029"}}>
+                    {item.comments.map((comment)=>{
+                      return(
+                        <p className="comm">
+                      <span className="commmenter" style={{fontWeight:"bolder"}}>{comment.postedBy.name}{" "}</span>
+                      <span className="commentText"> {comment.text}</span>
+                      </p>
+                      )
+                    })}
+                    
+                  </div>
 
                     {/*card-content*/}
               <div className="card-content">
-                    <p>2 Likes</p>
-                    <p>awesome post</p>
+                    <p>{item.likes.length}</p>
+                    <p>{item.body}</p>
               </div>
-             {/*add comment*/}
-           <div className="add-comment">
+            {/*add comment*/}
+            <div className="add-comment">
+              <span className="material-symbols-outlined">mood</span>
               <input type="text" placeholder="Add a comment" value={comment} onChange={(e)=>{setComment(e.target.value)}} />
               <button className="comment" 
-             // onClick={()=>{makeComment(posts._id, comment)}}
-              >Post</button>
+                onClick={()=>{makeComment(item._id, comment)
+                toggleComment()
+              }}>Post</button>
             </div>
               
             </div>
         </div>
+        <div className="close-comment">
+          <span className="material-symbols-outlined 
+            material-symbols-outlined-comment" 
+            onClick={()=>{toggleComment()}}>close</span>
+        </div>
       </div>
+        )}
     </div>
   );
 }
